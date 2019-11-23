a817653edcaf6df6a310272b6c7947ed45d60674
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-11-22 19:16:55 -0800

    User can submit a GitHub handle to have that handles repos added to the database

--- database/index.js
+++ database/index.js
@@ -6,16 +6,40 @@ let repoSchema = mongoose.Schema({
   owner: String,
   name: String,
   link: String,
-  stargazers: String,
+  stargazers: Number,
   description: String
 });
 
+repoSchema.path('gh_id').index({unique:true});
+
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
+let save = (data) => {
+  console.log('hello from database');
+  for (var result of data.items) {
+    var record = new Repo({
+      gh_id: result.id,
+      owner: result.owner.login,
+      name: result.name,
+      link: result.html_url,
+      stargazers: result.stargazers_count,
+      description: result.description
+    })
+    record.save((err, record) => {
+      if (err) return console.error(err);
+    })
+  }
   // This function should save a repo or repos to
   // the MongoDB
 }
 
+let get = (callback) => {
+  Repo.
+   find().
+   sort({stargazers: 'desc'}).
+   limit(25).
+   exec(callback);
+}
+
 module.exports.save = save;
+module.exports.get = get;
--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,22 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (term, callback) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
+  // https://api.github.com/search/repositories?q=user:jeckacodes
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/search/repositories?q=user:${term}`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${config.TOKEN}`,
+      'Accept': 'application/vnd.github.v3+json'
     }
   };
+  request(options, callback);
 
 }
 
